b10c9b5de76b17069cb3e67a54627b1a8c122bdc
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2024-07-10 11:34:35 -0700

    Added two files to the src folder: Added index.html Added script.js: Added gameBoardFactory and playerFactory objects inside

--- src/script.js
+++ src/script.js
@@ -1,31 +1,48 @@
+const gameBoardFactory = () => {
+    let board = [['', '', ''], ['', '', ''], ['', '', '']];
+
+    const setCell = (row, col, value) => {
+        if (board[row][col] === '') {
+            board[row][col] = value;
+        }
+    };
+
+    const getBoard = () => {
+        return board;
+    };
+    return { setCell, getBoard };
+};
+
+const playerFactory = (name, mark) => {
+    let player = {};
+    player.name = name;
+    player.mark = mark;
+    return player;
+};
+
+function getPlayerData(player) {
+    playerName = player.name;
+    playerMark = player.mark;
+    console.log(playerName);
+    console.log(playerMark);
+};
+
+// let namePlayer = prompt('Please enter your name: ');
+// let markPlayer = prompt('Please type either "x" or "o" mark for your move');
+// let player1 = playerFactory(namePlayer, markPlayer);
+// console.log(player1);
+
+let gameBoard = gameBoardFactory();
 
 
-// function gameBoard(playerName, playerMark, playerMove ) {
-//     arrayBoard = [['','',''], ['','',''], ['','','']];
-//     // arrayBoard.push(player)
-//     return arrayBoard;
-// };
-// const board = gameBoard();
-// const board1 = board[0][1];
-// board1.push('1');
-
-// function gameBoard(playerName, playerMark, playerMove) {
-//     let arrayBoard = [['', '', ''], ['', '', ''], ['', '', '']];
-    
-//     // Update the board at the specified position
-//     arrayBoard[playerMove.row][playerMove.col] = playerMark;
-    
-//     return arrayBoard;
-// }
-
-// // Example usage:
-// const playerMove = { row: 0, col: 1 };
-// const updatedBoard = gameBoard("John", "X", playerMove);
-
-// console.log(updatedBoard);
-
-function openPlayerDialog() {
-    document.getElementById('player-dialog').style.display = 'block';
-}
-
-window.onload = openPlayerDialog;
+// let move1 = playerMove(1, 0, 'x');
+// let move2 = playerMove(1, 1, 'x');
+// let move3 = playerMove(1, 2, 'x');
+// console.log(gameBoard.getBoard());
+// const getBoard = gameBoardFactory();
+// console.log(getBoard.getBoard());
+// console.log('-------------------------------')
+// const testCell = getBoard.setCell(1, 0, 'x');
+// const testCell1 = getBoard.setCell(1, 1, 'x');
+// const testCell2= getBoard.setCell(1, 2, 'x');
+// console.log(getBoard.getBoard())
